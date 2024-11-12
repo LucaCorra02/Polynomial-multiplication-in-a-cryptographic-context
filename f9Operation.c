21b6cac04f91bea42c79f131b7c16a678f9ddfdb
LucaCorra02 <luca.corradini4@gmail.com>
2024-11-12 16:42:39 +0100

    add f9 operation

--- f9Operation.c
+++ f9Operation.c
@@ -2,6 +2,11 @@
 #include "getTime.h"
 #include "utils.h"
 
+/*
+	make all
+	./f9Operation.out
+ */
+
 typedef struct f9_element {unsigned int real; unsigned int imaginary;} f9_element;
 
 void print_f9_element(f9_element n) {
@@ -23,9 +28,44 @@ f9_element f9_sum(f9_element a, f9_element b) {
     return ris;
 }
 
-int main(int argc, char *argv[]) { //ARGV = file_name , file_rows, num_operands
-  f9_element a = get_f9_element(150,-1);
-  f9_element b = get_f9_element(2,1);
-  f9_element ris = f9_sum(a,b);
-  print_f9_element(ris);
+f9_element f9_prod(f9_element a, f9_element b) {
+	unsigned int a1 = a.imaginary, a0 = a.real;
+	unsigned int b1 = b.imaginary, b0 = b.real;
+
+	unsigned int a0b1 = f3_prod(a0,b1);
+    unsigned int a1b0 = f3_prod(a1,b0);
+    unsigned int a0b0 = f3_prod(a0,b0);
+    unsigned int a1b1 = f3_prod(a1,b1);
+
+   	f9_element ris;
+    ris.real = f3_sum(a0b1,a1b0);
+	ris.imaginary = f3_sum(a0b0,abs_f3(a1b1));
+    return ris;
 }
+
+int main(int argc, char *argv[]) { //ARGV = file_name , file_rows, num_operands
+  /*
+	printf("%s %s\n",argv[1],argv[3]);
+  	f9_element a = get_f9_element(0,1);
+  	f9_element b = get_f9_element(2,0);
+  	f9_element ris = f9_prod(a,b);
+  	print_f9_element(ris);
+ */
+	if (argc < 4) {
+        fprintf(stderr, "No args\n");
+        return 1;
+    }
+    unsigned int file_rows = atoi(argv[2]), num_operands = atoi(argv[3]);
+    unsigned int operations[file_rows][num_operands];
+    if (read_file(argv[1],file_rows,num_operands, operations) != 0) return 1;
+
+    for (int i = 0; i < file_rows; i++) {
+    	f9_element a = get_f9_element(operations[i][0],operations[i][1]);
+        f9_element b = get_f9_element(operations[i][2],operations[i][3]);
+        f9_element ris = f9_prod(a,b);
+      	print_f9_element(ris);
+    }
+
+    //printf("A: (img: %d,real: %d), B: (img: %d,real: %d)\n", operations[3][0], operations[3][1], operations[3][2], operations[3][4]);
+}
+
--- utils.h
+++ utils.h
@@ -25,4 +25,31 @@ int load_vector(char *filename, unsigned int num_operations,unsigned int operand
     return 0;
 }
 
+int read_file(char *filename, unsigned int num_operations,unsigned int operands, unsigned int operations[num_operations][operands]) {
+    FILE *file = fopen(filename, "r");
+    if (file == NULL) {
+        perror("Errore apertura file");
+        return 1;
+    }
+    char line[100];
+    unsigned int cont = 0;
+    while (fgets(line, sizeof(line), file) != NULL) {
+        char *part1 = strtok(line, ";"), *part2 = strtok(NULL, ";");
+        if (part1 && part2) {
+            char *a1 = strtok(part1, ","), *a0 = strtok(NULL, ",");
+            char *b1 = strtok(part2, ","), *b0 = strtok(NULL, ",");
+            if (a1 && a0 && b1 && b0) {
+                int a1_img = atoi(a1), a0_real = atoi(a0);
+                int b1_img = atoi(b1), b0_real = atoi(b0);
+                operations[cont][0] = a1_img,operations[cont][1] = a0_real;
+                operations[cont][2] = b1_img,operations[cont][3] = b0_real;
+            }
+        }
+        cont++;
+    }
+    //printf("A: (img: %d,real: %d), B: (img: %d,real: %d)\n", operations[3][0], operations[3][1], operations[3][2], operations[3][4]);
+    fclose(file);
+    return 0;
+}
+
 
