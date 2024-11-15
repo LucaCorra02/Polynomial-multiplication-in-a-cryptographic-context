016caf250763cc5131bbf29941b93ec555702c6d
LucaCorra02 <luca.corradini4@gmail.com>
2024-11-15 14:07:47 +0100

    add benchmark function

--- f9Operation.c
+++ f9Operation.c
@@ -46,6 +46,25 @@ f9_element f9_prod(f9_element a, f9_element b) {
     return ris;
 }
 
+long double benchmark(f9_element f9_operation(f9_element a, f9_element b),unsigned int num_operations, unsigned int** operations){
+  	long double total_time = 0.0;
+    long double start_time = 0.0;
+    long double end_time = 0.0;
+    int i;
+
+    start_time = get_current_time();
+   	for (i=0; i < num_operations; i++) {
+		f9_element a = get_f9_element(operations[i][0],operations[i][1]);
+		f9_element b = get_f9_element(operations[i][2],operations[i][3]);
+		f9_element ris = f9_prod(a,b);
+		//print_f9_element(ris);
+	}
+    end_time = get_current_time();
+    total_time = end_time - start_time;
+    return total_time;
+}
+
+
 int main(int argc, char *argv[]) { //ARGV = file_name , file_rows, num_operands
 	if (argc < 4) {
         fprintf(stderr, "No args\n");
@@ -55,12 +74,8 @@ int main(int argc, char *argv[]) { //ARGV = file_name , file_rows, num_operands
 	unsigned int** operations = create_vector(file_rows, num_operands);
 
     if (read_file(argv[1],file_rows,operations) != 0) return 1;
-    for (int i = 0; i < file_rows; i++) {
-    	f9_element a = get_f9_element(operations[i][0],operations[i][1]);
-        f9_element b = get_f9_element(operations[i][2],operations[i][3]);
-        f9_element ris = f9_prod(a,b);
-		print_f9_element(ris);
-    }
+   	long double total_time = benchmark(f9_prod,file_rows, operations);
+    printf("%Lfs\n", total_time);
     free_vector(operations, file_rows);
     return 0;
 }
