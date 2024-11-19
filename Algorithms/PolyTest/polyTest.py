8de239fc5f7bcfc702d6e20d53d17f320b0ebee0
LucaCorra02 <luca.corradini4@gmail.com>
2024-11-19 13:45:24 +0100

    add polyTest generator

---
+++ Algorithms/PolyTest/polyTest.py
@@ -0,0 +1,57 @@
+import numpy as np
+import random
+import sys
+
+def gen_random_compleate_poly(max_terms,max_coff):
+    n_terms = random.randint(1, max_terms)
+    coff_a = []
+    coff_b = []
+    for i in range (0,n_terms):
+        term_a = random.randint(0, max_coff)
+        term_b = random.randint(0, max_coff)
+        coff_a.append(term_a)
+        coff_b.append(term_b)
+    return coff_a, coff_b
+
+def print_input(coff_a, coff_b):
+    print(*coff_a, sep=',',end='')
+    print(";",end='')
+    print(*coff_b, sep=',')
+
+def calc_prod(coff_a, coff_b):
+    coff_a.reverse(),coff_b.reverse()
+    p1d, p2d = np.poly1d(coff_a), np.poly1d(coff_b)
+    p1,p2 = np.polynomial.Polynomial(p1d.coef[::-1]),np.polynomial.Polynomial(p2d.coef[::-1])
+    #print("(",p1,")"," * ","(",p2,")")
+    ris = np.polymul(p1d,p2d)
+    #print(np.polynomial.Polynomial(ris.coef[::-1]))
+    return ris
+
+def print_prod(ris):
+    np.sort(ris)
+    rev = []
+    for i in range(0,len(ris)+1):
+        rev.append(ris[i])
+    print(*rev, sep=',')
+
+def gen_correct_output_prod(file_name):
+    with open(file_name, "r") as file:
+        for line in file:
+            tmp = line.strip().split(';')
+            coff_a = [int(coff) for coff in tmp[0].split(',')]
+            coff_b = [int(coff) for coff in tmp[1].split(',')]
+            print_prod(calc_prod(coff_a,coff_b))
+
+def gen_correct_input(rows,n_terms,max_coff):
+    for i in range (0,rows):
+        coff_a,coff_b = gen_random_compleate_poly(n_terms,max_coff)
+        print_input(coff_a,coff_b)
+
+def main():
+    #gen_correct_input(10000,10,1000)
+    args = sys.argv[1:]
+    gen_correct_output_prod(args[0])
+
+main()
+
+
