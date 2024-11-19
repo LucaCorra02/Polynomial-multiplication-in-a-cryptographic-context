0dc49b3afdfa8470fe8c6994b3ba48d41f20111c
LucaCorra02 <luca.corradini4@gmail.com>
2024-11-19 17:24:47 +0100

    rename file

--- Algorithms/Schoolbook/schoolbook.c
+++ Algorithms/Schoolbook/schoolbook.c
@@ -1,8 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include "../fileutils.h"
 
 //considero polinomi su 32 bit
 //funzione con grado pollinmimio, legge da un file che prodotto utilizzare
+
+/*
+    cd ..
+    make all
+    cd Schoolbook
+    ./schoolbook.out ../PolyTest/TestFile/LowDeg/lowDeg.txt 10000 10
+
+
+ */
 
 int* schoolbook_recursive(int n, int* p1, int* p2, int* ris){
     if(n==0){return ris;} //caso di uscita
@@ -26,6 +37,22 @@ int* schoolbook_r(int n, int* p1, int* p2) { //n = numero di termini.
     return schoolbook_recursive(n, p1, p2, ris);
 }
 
-int main() {
+int main(int argc, char *argv[]) {
 
+    if (argc < 4) {
+        fprintf(stderr, "No args\n");
+        return 1;
+    }
+    int num_rows = atoi(argv[2]), num_terms = atoi(argv[3]);
+    int** p1 = create_vector(num_rows,num_terms);
+    int** p2 = create_vector(num_rows,num_terms);
+
+    if(read_file(argv[1], num_rows, num_terms, p1, p2) != 0){return 1;};
+    for(int i = 0; i < num_rows; i++){
+        int* ris = schoolbook_r(num_terms, p1[i], p2[i]);
+        print_output_poly(ris,(num_terms*2)-1);
+    }
+    free_vector(p1, num_rows);
+    free_vector(p2, num_rows);
+    return 0;
 }
