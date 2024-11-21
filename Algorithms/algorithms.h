29c119290c7cfc87d5c88bb3206254554a7761fd
LucaCorra02 <luca.corradini4@gmail.com>
2024-11-21 15:05:15 +0100

    add header file with algorithms

---
+++ Algorithms/algorithms.h
@@ -0,0 +1,17 @@
+#ifndef ALGORITMS_H
+
+typedef int* (*NextAlgorithms)(int n, int* p1, int* p2);
+
+int* schoolbook_recursive(int n, int* p1, int* p2, int* ris);
+
+int* schoolbook_r(int n, int* p1, int* p2);
+
+int* karatsuba(int n, int* p1, int* p2);
+
+int* unbalanced_karatsuba(int n, int* p1, int* p2);
+
+NextAlgorithms choose_next_algo(int n, int* p1, int* p2);
+
+#define ALGORITMS_H
+
+#endif
