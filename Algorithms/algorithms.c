5ea2c805a02a59372c650ece3a2c51fc30045865
LucaCorra02 <luca.corradini4@gmail.com>
2024-11-22 17:37:14 +0100

    update schoolbook

--- Algorithms/algorithms.c
+++ Algorithms/algorithms.c
@@ -4,26 +4,28 @@
 #include "algorithms.h"
 
 
-int* schoolbook_recursive(int n, int* p1, int* p2, int* ris){
-    if(n==0){return ris;} //caso di uscita
-    int max_deg = n - 1;
-    int mst_p1 = p1[max_deg]; //termine di grado massimo
-    int mst_p2 = p2[max_deg];
-    ris[max_deg * 2] += mst_p1 * mst_p2;
+int* schoolbook_r(int n, int* p1, int* p2) {
+    int dim_ris = (2 * n) - 1;
+    int* ris = calloc(dim_ris, sizeof(int));
+    if (n == 1) { ris[0] = p1[0] * p2[0]; return ris; } // Caso base
 
-    for (int i = 0; i < max_deg; i++){
-        ris[max_deg + i] += mst_p1 * p2[i];
-    }
-    for (int i = 0; i < max_deg; i++){
+    int max_deg = n - 1;
+    int mst_p1 = p1[max_deg];
+    int mst_p2 = p2[max_deg];
+    ris[max_deg * 2] += mst_p1 * mst_p2; // Moltiplico termini di grado massimo
+    for (int i = 0; i < max_deg; i++) {
+        ris[max_deg + i] += mst_p1 * p2[i]; // Moltiplico il termine di grado massimo di p1 per p2
         ris[max_deg + i] += mst_p2 * p1[i];
     }
-    return schoolbook_recursive(n - 1, p1, p2, ris);
-}
 
-int* schoolbook_r(int n, int* p1, int* p2) { //n = numero di termini.
-    int* ris = calloc((2 * n) - 1, sizeof(int)); // Alloca e inizializza.
-    if (n==1) {ris[0] = p1[0] * p2[0]; return ris;} // Caso base, evito la chimata ricorsiva.
-    return schoolbook_recursive(n, p1, p2, ris);
+    NextAlgorithms next = choose_next_algo(n-1, p1, p2);
+    int* sub_result = next(n - 1, p1, p2);
+    for (int i = 0; i < (dim_ris - 1); i++) { // Combino i risultati
+        ris[i] += sub_result[i];
+    }
+
+    free(sub_result); // Libera la memoria temporanea usata nella ricorsione.
+    return ris;
 }
 
 int* karatsuba(int n, int* p1, int* p2) {
