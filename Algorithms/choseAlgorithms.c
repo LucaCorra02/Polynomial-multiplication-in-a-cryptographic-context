4da3531db96e8358d746d9add132cd9e4cacb2bb
LucaCorra02 <luca.corradini4@gmail.com>
2024-11-22 17:37:50 +0100

    remove print

--- Algorithms/choseAlgorithms.c
+++ Algorithms/choseAlgorithms.c
@@ -4,12 +4,10 @@
 #include "algorithms.h"
 
 NextAlgorithms choose_next_algo(int n, int* p1, int* p2){
-      if (n%2 == 0){
-        printf("Scelgo karatsuba con n = %d \n",n);
-        return karatsuba;
-      }
-      printf("Scelgo School con n = %d\n",n);
-      return schoolbook_r;
+  	if (n%2 == 0){
+    	return karatsuba;
+    }
+    return schoolbook_r;
 }
 
 
