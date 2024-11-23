b508a2a81f0fe00c4e000856ba744c589c2a7b60
LucaCorra02 <luca.corradini4@gmail.com>
2024-11-23 17:54:39 +0100

    add function

--- Algorithms/algorithms.h
+++ Algorithms/algorithms.h
@@ -4,6 +4,8 @@ typedef int* (*NextAlgorithms)(int n, int* p1, int* p2);
 
 NextAlgorithms choose_next_algo(int n, int* p1, int* p2);
 
+void polynomial_sum(int* A, int lenA, int* B, int lenB, int* result);
+
 int* schoolbook_r(int n, int* p1, int* p2);
 
 int* karatsuba(int n, int* p1, int* p2);
