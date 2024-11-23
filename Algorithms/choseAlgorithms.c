ab8177d7d4fa60faaebf0599fe37ce6e141abb2d
LucaCorra02 <luca.corradini4@gmail.com>
2024-11-23 17:55:07 +0100

    update

--- Algorithms/choseAlgorithms.c
+++ Algorithms/choseAlgorithms.c
@@ -5,9 +5,12 @@
 
 NextAlgorithms choose_next_algo(int n, int* p1, int* p2){
   	if (n%2 == 0){
+        //printf("Con n = %d, scelgo karatsuba\n",n);
     	return karatsuba;
     }
-    return schoolbook_r;
+    //return schoolbook_r
+    //printf("Con n = %d, scelgo unbalanced_karatsuba\n",n);
+    return unbalanced_karatsuba;
 }
 
 
