97128057871acbb972613972b36439ff9ab0e422
LucaCorra02 <luca.corradini4@gmail.com>
2024-12-02 13:35:03 +0100

    refactor

--- F3Implementation/basicOperationBool.c
+++ F3Implementation/basicOperationBool.c
@@ -6,26 +6,25 @@
 #include "../getTime.h"
 #include "../utils.h"
 
-/*
-    TODO : metodo chw switcha i bit ( fa il meno) .
-    TODO : valgrind massif
-    gcc -O3 basicOperationBool.c -o basicOperationBool
-    time ./basicOperationBool TestFile/testLowNumber.txt 1000000 2 > TestFile/outputSumLowNumber.txt
-*/
+typedef struct {
+    long double total_time;
+    long double mean_time;
+    int result;
+} results;
+
+typedef struct f3_element {bool bits[2];} f3_element;
 
 #define CHECK_BIT(n,pos) ((n) & (1<<(pos))) //macro per controllare se l'n-esimo bit è settato.
 
 const unsigned int LSB_I = 0; //la posizione del bit meno significativo
 const unsigned int MSB_I = 1; //posizione bit più significativo
 const unsigned int look_up_table[] = {0b00,0b01,0b10,0b00};
-typedef struct f3_element {bool bits[2];} f3_element; //Passo tutto per copia usando lo stack, più effecente che allocare sullo heap.
-// Incapsulo l'array in una struct in quanto è più comodo da passare alle funzioni.
 
-unsigned int f3_to_int(f3_element f3) {
+unsigned int f3_to_int_bool(f3_element f3){
     return look_up_table[f3.bits[MSB_I]*2+f3.bits[LSB_I]];
 }
 
-f3_element int_to_f3(unsigned int n){
+f3_element int_to_f3_bool(unsigned int n){
     n = n%3;
     f3_element element;
     element.bits[0] = 0, element.bits[1] = 0;
@@ -35,17 +34,17 @@ f3_element int_to_f3(unsigned int n){
 }
 
 void print_f3_element(f3_element f3){
-  printf("msb: %d, lsb: %d\n",f3.bits[MSB_I],f3.bits[LSB_I]);
+    printf("msb: %d, lsb: %d\n",f3.bits[MSB_I],f3.bits[LSB_I]);
 }
 
-void print_binary(unsigned int n) { //stampa rappresentazione binaria di un intero.
+void print_binary_bool(unsigned int n){
     unsigned int i, s = 1<<((sizeof(n)<<3)-1);
     printf("%d: ",n);
     for (i = s; i; i>>=1) printf("%d", n & i || 0 );
     printf("\n");
 }
 
-f3_element f3_sum(f3_element a, f3_element b){
+f3_element f3_sum_bool(f3_element a, f3_element b){
     f3_element ris;
     bool xor00 = a.bits[LSB_I] ^ b.bits[LSB_I];
     bool xor01 = a.bits[MSB_I] ^ b.bits[MSB_I];
@@ -58,7 +57,7 @@ f3_element f3_sum(f3_element a, f3_element b){
     return ris; //Mod 3;
 }
 
-f3_element f3_prod(f3_element a, f3_element b){
+f3_element f3_prod_bool(f3_element a, f3_element b){
     f3_element ris;
     bool and00 = a.bits[LSB_I] & b.bits[LSB_I];
     bool and03 = a.bits[MSB_I] & b.bits[MSB_I];
@@ -70,27 +69,40 @@ f3_element f3_prod(f3_element a, f3_element b){
     return ris; //Mod 3
 }
 
-long double benchmark(f3_element f3_operation(f3_element a, f3_element b),unsigned int num_operations, unsigned int operands, unsigned int **operations)
-{
-    //long double mean_time = 0.0;
+
+results benchmark_f3_bool(f3_element f3_operation(f3_element a, f3_element b),unsigned int num_operations, unsigned int** operations){
+    long double mean_time = 0.0;
     long double total_time = 0.0;
     long double start_time = 0.0;
     long double end_time = 0.0;
-    int i;
 
     start_time = get_current_time();
-    for(i = 0; i < num_operations; i++){
-        //printf("bool %d %d\n",operations[i][0],operations[i][1]);
-        f3_element ris = f3_operation(int_to_f3(operations[i][0]),int_to_f3(operations[i][1]));
-        printf("%d\n",f3_to_int(ris));
+    unsigned int total_ris = 0;
+    for(unsigned int i = 0; i < num_operations; i++){
+        //printf("%d %d\n",operations[i][0],operations[i][1]);
+        f3_element ris = f3_operation(int_to_f3_bool(operations[i][0]), int_to_f3_bool(operations[i][1]));
+        //printf("%d\n",f3_to_int_bool(ris));
+        total_ris += f3_to_int_bool(ris);
     }
     end_time = get_current_time();
     total_time = end_time - start_time;
-    // printf("Total time spent: %Lf\n", total_time);
-    // mean_time = total_time / (long double) i; //mean time troppo piccolo
-    // printf("Mean time spent: %Lf\n", mean_time);
-    return total_time;
+    mean_time = total_time / (long double) num_operations;
+    results results = {total_time, mean_time, total_ris};
+    return results;
 }
+
+void testFile(f3_element  f3_operation(f3_element a, f3_element  b),unsigned int file_rows, unsigned int** operations){
+    results ris = benchmark_f3_bool(f3_operation, file_rows, operations);
+}
+
+void bench(f3_element  f3_operation(f3_element  a, f3_element  b),unsigned int file_rows, unsigned int** operations){
+    int rip = 200;
+    for (int i = 1; i <= rip; i++) {
+        results ris = benchmark_f3_bool(f3_operation, file_rows, operations);
+        printf("Esecuzione: %d %d Mean Time %.14Lf -> Total time spent: %Lf\n", i ,ris.result, ris.mean_time , ris.total_time);
+    }
+}
+
 
 int main(int argc, char *argv[]) { //ARGV = file_name , file_rows, num_operands
     if (argc < 4) {
@@ -98,11 +110,18 @@ int main(int argc, char *argv[]) { //ARGV = file_name , file_rows, num_operands
         return 1;
     }
     unsigned int file_rows = atoi(argv[2]), num_operands = atoi(argv[3]);
-    unsigned int **operations = create_vector(file_rows,num_operands);
-    if (load_vector(argv[1],file_rows,operations) != 0){return 1;}
+    unsigned int** operations = create_vector(file_rows, num_operands);
+    if (load_vector(argv[1], file_rows, operations) !=0 ){ return 1; }
+    printf("op: %d\n",operations[0][0]);
 
-    //long double total_time = benchmark(f3_sum, file_rows, num_operands, operations);
-    long double total_time = benchmark(f3_prod, file_rows, num_operands, operations);
-    //printf("%Lfs\n", total_time);
+    //testFile(f3_sum, file_rows, operations);
+    //printf("ciao");
+    //testFile(f3_prod_bool, file_rows, operations);
+
+    //bench(f3_sum_bool, file_rows, operations);
+    bench(f3_prod_bool, file_rows, operations);
+
+
+    free_vector(operations, file_rows);
     return 0;
 }
