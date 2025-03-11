95a41d6cb15be641eb30ddf12567367d9a608dc6
LucaCorra02 <luca.corradini4@gmail.com>
2025-03-11 17:21:22 +0100

    add test f3 sum

--- PolyImplementation/benchmarks.cpp
+++ PolyImplementation/benchmarks.cpp
@@ -1,6 +1,5 @@
 extern "C" {
     #include "f3Utils.h"
-    #include "utils.h"
 }
 #include <benchmark/benchmark.h>
 #include <stdio.h>
@@ -11,24 +10,22 @@ static std::string PATH = "../TestFile/Cifra4/input.txt";
 static const unsigned int num_operators = 2;
 static const unsigned int num_operations = 1000000;
 
-static void DoSetup(const benchmark::State& state) {
-    operands= create_vector(num_operations,num_operators);
-    read_file((char*)PATH.c_str(), num_operations, operands);
-}
+//./benchmarks.out --benchmark_min_warmup_time 20s
 
-static void TearDown() {
-    free_vector(operands,num_operations);
-}
+static void Test_F3_Sum(benchmark::State& state){ //Testo tempo medio di un operazione.
+    unsigned int size = (unsigned int)state.range(0);
+    unsigned int a = 9;
+    unsigned int b = 9;
+    for (auto i = 0; i < size; i++){
+        a *= 10;
+        b *= 10;
+    }
 
-static void BM_TestBench(benchmark::State& state) {
-    int ris = f3_sum(10, 10);
-    //printf("ris = %d\n", ris);
-    for (auto _ : state)
-    {
-        benchmark::DoNotOptimize(f3_sum(0, 2));
+    for (auto _ : state){
+        benchmark::DoNotOptimize(f3_sum(int_to_f3(a), int_to_f3(b)));
     }
 }
 
-BENCHMARK(BM_TestBench)->Setup(DoSetup);
+BENCHMARK(Test_F3_Sum)->DenseRange(1, 10, 1);
 
 BENCHMARK_MAIN();
