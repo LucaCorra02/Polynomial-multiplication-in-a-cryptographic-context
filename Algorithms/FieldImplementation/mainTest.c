8f2185ef96bb6029bdea5bb09b1426de3edf2686
LucaCorra02 <luca.corradini4@gmail.com>
2025-03-24 17:14:09 +0100

    pass test swap_bits

--- Algorithms/FieldImplementation/mainTest.c
+++ Algorithms/FieldImplementation/mainTest.c
@@ -1,21 +1,52 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#include "../../munit/munit.h"
 #include "f3Utils.h"
 #include "f9Utils.h"
 
-int main(int argc, char *argv[]){
-    int a = 3;
-    int b = 2;
-    unsigned int f3_a = int_to_f3(a);
-    unsigned int f3_b = int_to_f3(b);
-    unsigned int ris = f3_sum(f3_a, f3_b);
-    printf("ris = %d\n", ris);
+/*
+ * Le funzioni di test ritornano MUNIT_OK se il test passa,
+ * altrimenti generano un'asserzione fallita.
+ */
 
-    f9_element f9_a = f3_a;
-    f9_element f9_b = f3_b;
-    print_f9_element(f3_b);
+static MunitResult test_kthbit(const MunitParameter params[], void* user_data) {
+    (void) params;
+    (void) user_data;
 
-    return 0;
+    int input[] = {0b1011, 0b1010, 0b1110};
+    int expected[] = {0b1, 0b0, 0b0};
+    int dim = 3;
+
+    for (int i = 0; i < dim; ++i) {
+        int ris = kth_bit(input[i], 0);
+        munit_assert_int(ris, ==, expected[i]);
+    }
+    return MUNIT_OK;
 }
 
+static MunitResult test_swapbits(const MunitParameter params[], void* user_data) {
+    (void) params;
+    (void) user_data;
+
+    int input[] = {0b00, 0b01, 0b10, 0b11};
+    int expected[] = {0b00, 0b10, 0b01, 0b00};
+    int dim = 4;
+
+    for (int i = 0; i < dim; ++i){
+        int ris = swap_bits(input[i]);
+        munit_assert_int(ris, ==, expected[i]);
+    }
+    return MUNIT_OK;
+}
+
+static MunitTest tests[] = {
+    { "/test_kthbit", test_kthbit, NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL },
+    { "/test_swapbits", test_swapbits, NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL },
+    { NULL, NULL, NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL }
+};
+
+static const MunitSuite suite = {
+    "/f3Utils_tests", tests, NULL, 1, MUNIT_SUITE_OPTION_NONE
+};
+
+int main(int argc, char* argv[]) {
+    return munit_suite_main(&suite, NULL, argc, argv);
+}
