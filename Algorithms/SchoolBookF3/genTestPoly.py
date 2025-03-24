47dbc1c76338badf3909e80ff389637b655a181a
LucaCorra02 <luca.corradini4@gmail.com>
2025-03-24 11:54:50 +0100

    fix output format

--- Algorithms/SchoolBookF3/genTestPoly.py
+++ Algorithms/SchoolBookF3/genTestPoly.py
@@ -38,7 +38,7 @@ def gen_correct_output_prod(file_name):
             print_cof_formatted(ris)
 
 def main():
-    num_rows = 1
+    num_rows = 500
     num_terms = 1024
     min_range = 0
     max_range = 1000
