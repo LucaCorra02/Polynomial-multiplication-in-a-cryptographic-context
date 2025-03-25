186a2a497b4f2b5ca4e35d21acf6f455a937dc07
LucaCorra02 <luca.corradini4@gmail.com>
2025-03-25 11:40:48 +0100

    refactor getter test f9

--- Algorithms/FieldImplementation/mainTest.c
+++ Algorithms/FieldImplementation/mainTest.c
@@ -99,17 +99,20 @@ static MunitResult test_get_f9_element(const MunitParameter params[], void* user
     return MUNIT_OK;
 }
 
-static MunitResult test_get_imaginary_part(const MunitParameter params[], void* user_data) {
+static MunitResult test_getter_f9(const MunitParameter params[], void* user_data) {
     (void) params;
     (void) user_data;
 
     f9_element f9[] = {get_f9_element(0,0), get_f9_element(1,-1), get_f9_element(-1,-2), get_f9_element(7,-3) };
-    int expected[] = {0b00, 0b01, 0b10, 0b01};
+    int expected_img[] = {0, 1, 2, 1};
+    int expected_real[] = {0, 2, 1, 0};
     int dim = 4;
 
     for (int i = 0; i < dim; ++i){
-        unsigned int ris = get_imaginary_part(f9[i]) ;
-        munit_assert_int(ris, ==, expected[i]);
+        unsigned int ris_img = get_imaginary_part(f9[i]);
+        unsigned int ris_real = get_real_part(f9[i]);
+        munit_assert_int(ris_img, ==, expected_img[i]);
+        munit_assert_int(ris_real, ==, expected_real[i]);
     }
     return MUNIT_OK;
 }
@@ -121,7 +124,7 @@ static MunitTest tests[] = {
     { "/test_int_f3_sum", test_f3_sum, NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL },
     { "/test_f3_prod", test_f3_prod, NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL },
     { "/test_get_f9_element", test_get_f9_element, NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL },
-    { "/test_get_imaginary_part", test_get_imaginary_part, NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL },
+    { "/test_getter_f9", test_getter_f9, NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL },
     { NULL, NULL, NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL }
 };
 
