e4ce99384c8f62448ecce9ad52355bf0dd643148
LucaCorra02 <luca.corradini4@gmail.com>
2025-03-31 12:37:13 +0200

    fix main const

--- Algorithms/Karatsuba/karatsubaField.c
+++ Algorithms/Karatsuba/karatsubaField.c
@@ -148,8 +148,8 @@ void print_vector(int* v, int num_elements){
     printf("%d\n",v[num_elements-1]);
 }
 
-#define BUFFERSIZE 3000;
-#define NUM_OPERANDS 33;
+#define BUFFERSIZE 100000
+#define NUM_OPERANDS 1024
 
 int main(int argc, char *argv[]){
 
