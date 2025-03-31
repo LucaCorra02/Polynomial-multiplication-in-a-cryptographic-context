38e6a253fc5744c079fb7bf3f0d80a62e6ae8e31
LucaCorra02 <luca.corradini4@gmail.com>
2025-03-31 18:03:47 +0200

    add benchmark f3

--- Algorithms/Karatsuba/benchmarks.cpp
+++ Algorithms/Karatsuba/benchmarks.cpp
@@ -4,13 +4,39 @@ extern "C" {
 
 #include <stdio.h>
 #include <string.h>
+#include <iostream>
 #include "../../benchmark/include/benchmark/benchmark.h"
 
 
 static unsigned int** operands;
 
+void printArray(int arr[], int size) {
+    std::cout << "[ ";
+    for (int i = 0; i < size; i++) {
+        std::cout << arr[i];
+        if (i < size - 1) std::cout << ", ";
+    }
+    std::cout << " ]" << std::endl;
+}
 
 //./benchmarks.out --benchmark_min_warmup_time=20s
+static void Benchmark_Karatsuba_F3(benchmark::State& state){ //Testo F9_shoolbook
+    int size = static_cast<int>(state.range(0));
+
+    int* p1 = new int[size];
+    int* p2 = new int[size];
+
+    for (int i = 0; i < size; ++i){
+        p1[i] = (i * 2) % 3;
+        p2[i] = (i + 1) % 3;
+    }
+    for (auto _ : state) {
+        benchmark::DoNotOptimize(unbalanced_karatsuba_f3(size, p1, p2));
+    }
+    delete[] p1;
+    delete[] p2;
+}
+
 
 static void Benchmark_Karatsuba_F9(benchmark::State& state){ //Testo F9_shoolbook
     int size = static_cast<int>(state.range(0));
@@ -30,6 +56,7 @@ static void Benchmark_Karatsuba_F9(benchmark::State& state){ //Testo F9_shoolboo
     delete[] p2;
 }
 
-BENCHMARK(Benchmark_Karatsuba_F9)->DenseRange(10, 2048, 100)->Unit(benchmark::kMillisecond);
+//BENCHMARK(Benchmark_Karatsuba_F9)->DenseRange(10, 2048, 100)->Unit(benchmark::kMillisecond);
+BENCHMARK(Benchmark_Karatsuba_F3)->DenseRange(10, 2048, 100)->Unit(benchmark::kMillisecond);
 
 BENCHMARK_MAIN();
