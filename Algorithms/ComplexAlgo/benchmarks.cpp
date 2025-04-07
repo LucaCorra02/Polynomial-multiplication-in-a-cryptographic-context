b6d3ce42a31d9ac1e3c9ba179053a63b6bce84f4
LucaCorra02 <luca.corradini4@gmail.com>
2025-04-07 15:15:18 +0200

    add 3split_f3 benchmarks

--- Algorithms/ComplexAlgo/benchmarks.cpp
+++ Algorithms/ComplexAlgo/benchmarks.cpp
@@ -8,7 +8,7 @@ extern "C" {
 
 //./benchmarks.out --benchmark_min_warmup_time=20s
 
-static void Benchmark_3split_f9(benchmark::State& state){ //Testo F9_shoolbook
+static void Benchmark_3split_f9(benchmark::State& state){ //Testo F9_split3
     int size = static_cast<int>(state.range(0));
 
     f9_element* p1 = new f9_element[size];
@@ -26,6 +26,25 @@ static void Benchmark_3split_f9(benchmark::State& state){ //Testo F9_shoolbook
     delete[] p2;
 }
 
-BENCHMARK(Benchmark_3split_f9)->DenseRange(10, 2048, 100)->Unit(benchmark::kMillisecond);
+static void Benchmark_3split_f3(benchmark::State& state){ //Testo F3_Split3
+    int size = static_cast<int>(state.range(0));
+
+    int* p1 = new int[size];
+    int* p2 = new int[size];
+
+    for (int i = 0; i < size; ++i){
+        p1[i] = (i + 1) % 3;
+        p2[i] = (i * 3) % 3;
+    }
+
+    for (auto _ : state) {
+        benchmark::DoNotOptimize(split_3_f3(size, p1, p2));
+    }
+    delete[] p1;
+    delete[] p2;
+}
+
+//BENCHMARK(Benchmark_3split_f9)->DenseRange(10, 2048, 100)->Unit(benchmark::kMillisecond);
+BENCHMARK(Benchmark_3split_f3)->DenseRange(10, 2048, 100)->Unit(benchmark::kMillisecond);
 
 BENCHMARK_MAIN();
