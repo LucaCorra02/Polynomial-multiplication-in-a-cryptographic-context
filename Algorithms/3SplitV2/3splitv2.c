443391f0177525df95a4e990ec31364341a7a7ad
LucaCorra02 <luca.corradini4@gmail.com>
2025-04-09 16:41:00 +0200

    work subproduct f3

--- Algorithms/3SplitV2/3splitv2.c
+++ Algorithms/3SplitV2/3splitv2.c
@@ -309,61 +309,27 @@ int* split_3_v2_f3(int m, int* p1, int* p2){
     printf("op_pointer_f9: ");
     print_vector_f9(op_pointer_f9, n * 2);
 
-
-
-    /*
-    int* S1 = op_pointer;
-    sum_poly_f3(n, k, A0, A2, S1); // S1 = A0 + A2
-    int* S2 = S1 + n;
-    sum_poly_f3(n, n, S1, A1, S2); // S2 = S1 + A1
-    int* S3 = S2 + n;
-    diff_poly_f3(n, n, S1, A1, S3); // S3 = S1 - A1
-    int* S4 = S3 + n;
-    diff_poly_f3(n, k, A0, A2, S4); // S4 = A0 - A2
-    f9_element* S5 = op_pointer_f9;
-    sum_poly_img_f3(n, n, S4, A1, S5); // S5 = S4 + A1w
-
-    int* S1_b = S4 + n ;
-    sum_poly_f3(n, k, B0, B2, S1_b); // S1_b = B0 + B2
-    int* S2_b = S1_b + n;
-    sum_poly_f3(n, n, S1_b, B1, S2_b); // S2_b = S1_b + B1
-    int* S3_b = S2_b + n;
-    diff_poly_f3(n, n, S1_b, B1, S3_b); // S3_b = S1_b - B1
-    int* S4_b = S3_b + n;
-    diff_poly_f3(n, k, B0, B2, S4_b); // S4_b = B0 - B2
-    f9_element* S5_b = S5 + n;
-    sum_poly_img_f3(n, n, S4_b, B1, S5_b); // S5_b = S4_b + B1w
-
-    int *P0, *P1, *P2, *P4;
-    f9_element *P3;
-    #pragma omp parallel sections
-    {
-       #pragma omp section
-       P0 = split_3_f3(n, A0, B0);
-       #pragma omp section
-       P1 = split_3_f3(n, S2, S2_b);
-       #pragma omp section
-       P2 = split_3_f3(n, S3, S3_b);
-       #pragma omp section
-       P3 = split_3_f9(n, S5, S5_b);
-       #pragma omp section
-       P4 = split_3_f3(k, A2, B2);
-    }
+	int *P0, *P1, *P4;
+    f9_element *P2;
+    P0 = schoolbook_f3(n, A0, B0); // P0 = A0*B0
+    P1 = schoolbook_f3(n, S4, S4_b); // P1 = S4 * S4_b
+    P2 = schoolbook_f9(n, S2, S2_b); //P2 = S2 * S2_b Su F9
+    P4 = schoolbook_f3(k, A2, B2); //P4 = A2 * B2
 
     int dim_subproduct = (2*n-1);
     int dim_subproduct_rem = (2*k-1);
 
-    int* Q1 = S4_b + n;
-    diff_poly_f3(dim_subproduct, dim_subproduct, P1, P2, Q1); // Q1 = P1 - P2
-    int* Q2 = Q1 + dim_subproduct;
-    diff_poly_img_f3(dim_subproduct, dim_subproduct, Q1, P3, Q2); // Q2 = Q1 - P3,1
-    int* Q3 = Q2 + dim_subproduct;
-    sum_poly_imag_f3(dim_subproduct, dim_subproduct, Q1, P3, Q3); // Q3 = Q1 + P3,1
-    int* Q4 = Q3 + dim_subproduct;
-    diff_poly_real_f3(dim_subproduct, dim_subproduct, P0, P3, Q4); // Q4 = P0 - P3,0
-    int* Q5 = Q4 + dim_subproduct;
-    sum_poly_f3(dim_subproduct, dim_subproduct_rem, Q4, P4, Q5); // Q5 = Q4 + P4
+    printf("P0: ");
+    print_vector_f3(P0, dim_subproduct);
+    printf("P1: ");
+    print_vector_f3(P1, dim_subproduct);
+    printf("P2:");
+    print_vector_f9(P2, dim_subproduct);
+    printf("P4:");
+    print_vector_f3(P4, dim_subproduct_rem);
 
+
+    /*
     int dim_ris = (2*m-1);
     int* ris = calloc(dim_ris, sizeof(int));
 
