53fe8fa3b130d4d17b06233c599638b19544fb5c
LucaCorra02 <luca.corradini4@gmail.com>
2025-04-15 11:23:50 +0200

    pass test test_neg_sum_poly_img

--- Algorithms/4Split/4split.h
+++ Algorithms/4Split/4split.h
@@ -33,6 +33,8 @@
 
     void sum_poly_img(int terms_p1, int terms_p2, f9_element* p1, f9_element* p2, f9_element* ris); // P1 + wP2
 
+    void neg_sum_poly_img(int terms_p1, int terms_p2, f9_element* p1, f9_element* p2, f9_element* ris); // -P1 + wP2
+
     void sum_poly_img_neg(int terms_p1, int terms_p2, f9_element* p1, f9_element* p2, f9_element* ris); //P1 - wP2
 
     f9_element* schoolbook_f9(int n, f9_element* p1, f9_element* p2);
