d55b1737b12c0f068218ee9b869b827ab761ddf7
LucaCorra02 <luca.corradini4@gmail.com>
2025-04-18 11:01:02 +0200

    refactor schoolbook f9

--- Algorithms/algorithms.c
+++ Algorithms/algorithms.c
@@ -5,7 +5,7 @@ f9_element* schoolbook_f9(int n, f9_element* p1, f9_element* p2) { //coefficenti
     int dim_ris = (2 * n) - 1;
     f9_element* ris = calloc(dim_ris, sizeof(f9_element));
     if (n == 1){
-        ris[0] = f9_prod(int_to_f9_element(p1[0]),int_to_f9_element(p2[0]));
+        ris[0] = f9_prod(p1[0],p2[0]);
         return ris;
     }
     int max_deg = n - 1;
@@ -14,16 +14,14 @@ f9_element* schoolbook_f9(int n, f9_element* p1, f9_element* p2) { //coefficenti
     ris[max_deg * 2] = f9_sum(ris[max_deg * 2], f9_prod(mst_p1,mst_p2));
 
     for (int i = 0; i < max_deg; i++) {
-        ris[max_deg + i] = f9_sum(int_to_f9_element(ris[max_deg + i]), f9_prod(mst_p1, int_to_f9_element(p2[i])));
-        ris[max_deg + i] = f9_sum(int_to_f9_element(ris[max_deg + i]), f9_prod(mst_p2, int_to_f9_element(p1[i])));
+        ris[max_deg + i] = f9_sum(ris[max_deg + i], f9_prod(mst_p1, p2[i]));
+        ris[max_deg + i] = f9_sum(ris[max_deg + i], f9_prod(mst_p2, p1[i]));
     }
-
     //print_vector(ris,dim_ris);
     f9_element* sub_result = schoolbook_f9(n - 1, p1, p2);
     for (int i = 0; i < (dim_ris - 1); i++) {
-        ris[i] = f9_sum(int_to_f9_element(ris[i]),int_to_f9_element(sub_result[i]));
+        ris[i] = f9_sum(ris[i],sub_result[i]);
     }
-
     free(sub_result);
     return ris;
 }
