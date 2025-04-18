5785d30cbef9890b1225b0ede9556265e7e13f7b
LucaCorra02 <luca.corradini4@gmail.com>
2025-04-18 11:26:25 +0200

    work 3 split v1

--- Algorithms/algorithms.h
+++ Algorithms/algorithms.h
@@ -6,6 +6,10 @@ int* schoolbook_f3(int n, int* p1, int* p2);
 
 f9_element* schoolbook_f9(int n, f9_element* p1, f9_element* p2);
 
+/* 3_SPLIT_V1 ALGO */
+int* split_3_f3(int m, int* p1, int* p2);
+
+f9_element* split_3_f9(int m, f9_element* p1, f9_element* p2);
 
 /* 3_SPLIT_V2 ALGO*/
 int* split_3_v2_f3(int m, int* p1, int* p2);
