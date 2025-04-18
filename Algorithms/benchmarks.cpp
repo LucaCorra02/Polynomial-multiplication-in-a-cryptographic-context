f6ac165a7cec910cc075ab8a42ba8d01c3599621
LucaCorra02 <luca.corradini4@gmail.com>
2025-04-18 14:06:22 +0200

    add count cycle benchmarks

--- Algorithms/benchmarks.cpp
+++ Algorithms/benchmarks.cpp
@@ -4,6 +4,7 @@ extern "C" {
 
 #include <stdio.h>
 #include <string.h>
+#include <x86intrin.h>  // Per __rdtsc()
 #include "../benchmark/include/benchmark/benchmark.h"
 
 //./benchmarks.out --benchmark_min_warmup_time=2 --benchmark_out=File/3splitf3.json --benchmark_out_format=json
@@ -25,6 +26,37 @@ static void Benchmark_4split_v2_f9(benchmark::State& state){ //Testo F9_split3
     delete[] p1;
     delete[] p2;
 }
+
+static void Benchmark_cpu_cycle_f9(benchmark::State& state) {
+    int size = static_cast<int>(state.range(0));
+
+    f9_element* p1 = new f9_element[size];
+    f9_element* p2 = new f9_element[size];
+
+    for (int i = 0; i < size; ++i) {
+        p1[i] = get_f9_element(i % 3, (i * 2) % 3);
+        p2[i] = get_f9_element((i * 3) % 3, (i + 1) % 3);
+    }
+
+    unsigned long long total_cycles = 0;
+    const int inner_loops = 100;
+
+    for (auto _ : state) {
+        unsigned long long start = __rdtsc();
+        for (int j = 0; j < inner_loops; ++j) {
+            benchmark::DoNotOptimize(split_4_f9(size, p1, p2));
+        }
+        unsigned long long end = __rdtsc();
+        total_cycles += (end - start);
+    }
+
+    delete[] p1;
+    delete[] p2;
+
+    double avg_cycles = static_cast<double>(total_cycles) / (state.iterations() * inner_loops);
+    state.counters["AvgCycles"] = benchmark::Counter(avg_cycles);
+}
+
 
 static void Benchmark_4split_f3(benchmark::State& state){
     int size = static_cast<int>(state.range(0));
