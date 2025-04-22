fc54ddd6cb07afe1b679f281f2e5974118baa159
LucaCorra02 <luca.corradini4@gmail.com>
2025-04-22 14:40:56 +0200

    pass test test_diff_poly_double_img_neg

--- Algorithms/algoUtils.c
+++ Algorithms/algoUtils.c
@@ -66,6 +66,11 @@ void sum_poly_img_neg(int terms_p1, int terms_p2, f9_element* p1, f9_element* p2
     for(int i = 0; i < terms_p2; i++){ ris[i] = f9_sum(ris[i], f9_prod_img_neg(p2[i])); }
 }
 
+void diff_poly_double_img_neg(int terms_p1, int terms_p2, f9_element* p1, f9_element* p2, f9_element* ris){ //-P1 - wP2
+    for(int i = 0; i < terms_p1; i++){ ris[i] = f9_sum(ris[i], f9_neg(p1[i])); }
+    for(int i = 0; i < terms_p2; i++){ ris[i] = f9_sum(ris[i], f9_prod_img_neg(p2[i])); }
+}
+
 void print_vector_f3(int* v, int num_elements){
     for (int i = 0; i < num_elements-1; i++){
         printf("%d,",v[i]);
