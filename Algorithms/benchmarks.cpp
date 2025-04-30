5a7d7cd2e2dcc6ebcbfd222339b0f052c61c3654
LucaCorra02 <luca.corradini4@gmail.com>
2025-04-30 15:26:34 +0200

    add benchmark f3

--- Algorithms/benchmarks.cpp
+++ Algorithms/benchmarks.cpp
@@ -4,8 +4,10 @@ extern "C" {
 
 #include <stdio.h>
 #include <string.h>
+#include <iostream>
 #include <x86intrin.h>  // Per __rdtsc()
 #include "../benchmark/include/benchmark/benchmark.h"
+
 
 //./benchmarks.out --benchmark_min_warmup_time=2 --benchmark_out=File/3splitf3.json --benchmark_out_format=json
 
@@ -76,9 +78,58 @@ static void Benchmark_4split_f3(benchmark::State& state){
     delete[] p2;
 }
 
+std::string selected_algo = "split_5_f3";
+
+static void BenchmarkF3(benchmark::State& state) {
+    int size = static_cast<int>(state.range(0));
+    int* p1 = new int[size];
+    int* p2 = new int[size];
+
+    for (int i = 0; i < size; ++i){
+        p1[i] = (i + 1) % 3;
+        p2[i] = (i * 3) % 3;
+    }
+
+    for (auto _ : state) {
+        if (selected_algo == "schoolbook_f3") {
+            benchmark::DoNotOptimize(schoolbook_f3(size, p1, p2));
+        } else if (selected_algo == "split_3_f3") {
+            benchmark::DoNotOptimize(split_3_f3(size, p1, p2));
+        } else if (selected_algo == "split_3_v2_f3") {
+            benchmark::DoNotOptimize(split_3_v2_f3(size, p1, p2));
+        } else if (selected_algo == "split_4_f3") {
+            benchmark::DoNotOptimize(split_4_f3(size, p1, p2));
+        } else if (selected_algo == "split_4_v1_f3") {
+            benchmark::DoNotOptimize(split_4_v1_f3(size, p1, p2));
+        } else if (selected_algo == "split_5_f3") {
+            benchmark::DoNotOptimize(split_5_f3(size, p1, p2));
+        } else {
+            std::cerr << "Algoritmo non riconosciuto: " << selected_algo << std::endl;
+            break;
+        }
+    }
+
+    delete[] p1;
+    delete[] p2;
+}
+
+BENCHMARK(BenchmarkF3)->DenseRange(30, 400, 10)->Unit(benchmark::kMillisecond);
+
+int main(int argc, char** argv) {
+    // Parsing manuale di --algo=...
+    for (int i = 1; i < argc; ++i) {
+        std::string arg = argv[i];
+        if (arg.rfind("--algo=", 0) == 0) {
+            selected_algo = arg.substr(7);
+        }
+    }
+
+    ::benchmark::Initialize(&argc, argv);
+    ::benchmark::RunSpecifiedBenchmarks();
+    return 0;
+}
 
 //BENCHMARK(Benchmark_4split_v2_f9)->DenseRange(10, 3048, 100)->Unit(benchmark::kMillisecond);
-BENCHMARK(Benchmark_4split_f3)->DenseRange(10, 3048, 100)->Unit(benchmark::kMillisecond);
-
-BENCHMARK_MAIN();
+//BENCHMARK(Benchmark_4split_f3)->DenseRange(10, 3048, 100)->Unit(benchmark::kMillisecond);
+//BENCHMARK_MAIN();
 
