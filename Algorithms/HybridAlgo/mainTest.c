0fbe9f84b6b71a5c5a846ffd8b6b0e2336157ec3
LucaCorra02 <luca.corradini4@gmail.com>
2025-05-11 11:21:54 +0200

    update f3 main

--- Algorithms/HybridAlgo/mainTest.c
+++ Algorithms/HybridAlgo/mainTest.c
@@ -740,14 +740,14 @@ int* split_operands_f3(char* p, int num_operands){
 }
 
 #define BUFFERSIZE 100000
-#define NUM_OPERANDS 761
+#define NUM_OPERANDS 27
 
 int main(int argc, char* argv[]) {
     //munit_suite_main(&suite, NULL, argc, argv);
 	//munit_suite_main(&suite_f3, NULL, argc, argv);
 
 
-	read_file_f9("f9_min.txt", 762);
+	/*read_file_f9("f9_min.txt", 762);
 	char buffer[BUFFERSIZE];
     while (fgets(buffer, BUFFERSIZE , stdin)){
         char* left = strtok(buffer, ";");
@@ -764,22 +764,26 @@ int main(int argc, char* argv[]) {
     printf("dim poly: %d\n", NUM_OPERANDS);
     printf("last algo id: %d\n", list_f9[760]); //-1 rispetto agli indici di f9_min.txt
     free(list_f9);
+    */
 
-    /*read_file_f3("prova.txt", 1024);
+    read_file_f3("f3_min.txt", 28);
+    read_file_f9("f9_min.txt", 28);
     char buffer[BUFFERSIZE];
     while (fgets(buffer, BUFFERSIZE , stdin)){
         char* left = strtok(buffer, ";");
         char* right = strtok(NULL, ";");
         int* p1 = split_operands_f3(left,NUM_OPERANDS);
         int* p2 = split_operands_f3(right,NUM_OPERANDS);
-        int* ris = schoolbook_f3(NUM_OPERANDS, p1,p2);
+        NextAlgoF3 chosen_1 = choose_next_algo_f3(NUM_OPERANDS);
+        int* ris = chosen_1(NUM_OPERANDS, p1,p2);
         print_vector_f3(ris,(2*NUM_OPERANDS)-1);
         free(p1);
         free(p2);
         free(ris);
     }
     printf("dim poly: %d\n", NUM_OPERANDS);
-	*/
+    free(list_f9);
+    free(list_f3);
     return 0;
 
 }
